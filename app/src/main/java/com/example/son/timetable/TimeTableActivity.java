8d0121b5e9f5704428906d4f26c8de9b9133b16d
JinYong <alwa09@daum.net>
2017-06-05 21:41:36 +0900

    테이블 입력부분 수정중 (지훈)

--- app/src/main/java/com/example/son/timetable/TimeTableActivity.java
+++ app/src/main/java/com/example/son/timetable/TimeTableActivity.java
@@ -1,13 +1,17 @@
 package com.example.son.timetable;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class TimeTableActivity extends AppCompatActivity {
@@ -23,6 +27,7 @@ public class TimeTableActivity extends AppCompatActivity {
             String button_id = getResources().getResourceName(v.getId());
             String[] values = button_id.split("_");
             Log.d(tag, values[0] + " " + values[1]);
+            showTimeTableDialog(values[0], values[1]);
             //insert(values[0], values[1], "테스트");
         }
     };
@@ -42,18 +47,41 @@ public class TimeTableActivity extends AppCompatActivity {
             Log.e(tag, "데이터베이스를 얻어올 수 없음");
             finish();
         }
-        insert("class1", "monday", "자바");
+
         select();
         updateTimeTable();
         registButtonFunc();
     }
 
-    void insert(String _class, String day, String lecture)
+    void showTimeTableDialog(final String _class, final String day) // 입력창 띄우기
+    {
+        final EditText edittext = new EditText(this);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("시간표 입력");
+        alertDialog.setMessage("시간표를 입력하세요.");
+        alertDialog.setView(edittext);
+        alertDialog.setPositiveButton("입력", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                insert(_class, day, edittext.getText().toString());
+                updateTimeTable();
+            }
+        });
+        alertDialog.setNeutralButton("삭제", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.show();
+    }
+
+    void insert(String _class, String day, String lecture) // DB에 삽입
     {
         db.execSQL("insert into timetable VALUES(null, '" + _class + "','" + day + "','" + lecture + "');");
     }
 
-    void select()
+    void select() // 임시 기능임
     {
         Cursor c = db.rawQuery("select * from timetable;", null);
         while(c.moveToNext())
