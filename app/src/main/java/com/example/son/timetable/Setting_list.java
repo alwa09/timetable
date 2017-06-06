71a925f3d7a57f11b808f9f6cfc145d725a98796
jeonjaesoo <wjswotn159@hanmail.net>
2017-06-06 18:10:14 +0900

    Setting layout 수정

--- app/src/main/java/com/example/son/timetable/Setting_list.java
+++ app/src/main/java/com/example/son/timetable/Setting_list.java
@@ -3,7 +3,6 @@ package com.example.son.timetable;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,32 +13,26 @@ import android.widget.ListView;
  */
 
 public class Setting_list extends AppCompatActivity{
-    static final String[] List_Menu = {"서비스", "서비스 종료", "장소 등록", "장소 삭제"};
+    static final String[] menu_list = {"서비스", "장소 등록", "장소 삭제"};
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
         getSupportActionBar().setTitle("Setting"); //title 이름
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, List_Menu);
-        ListView listview = (ListView)findViewById(R.id.setting);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, menu_list);
+        ListView listview = (ListView) findViewById(R.id.setting);
         listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String strText = (String)parent.getItemAtPosition(position);
-
                 if(strText.equals("서비스")){
-                    Log.i("aaa", "Service");
-                    Intent Service_intent = new Intent(getApplicationContext(), MyService.class);
-                    startService(Service_intent);
+                    Intent Service_intent = new Intent(getApplicationContext(), Switch_check.class);
+                    startActivity(Service_intent);
                 }
-                else if(strText.equals("서비스 종료"))
-                {
-                    Intent Service_intent = new Intent(getApplicationContext(), MyService.class);
-                    stopService(Service_intent);
-                }else if(strText.equals("장소 등록"))
+                else if(strText.equals("장소 등록"))
                 {
                     Intent placeRegister = new Intent(getApplicationContext(), RegisterPlace.class);
                     startActivity(placeRegister);
