af25356726afca08fea366945843a1344a68c02f
손지훈 <sjh0696@gmail.com>
2017-06-09 19:16:49 +0900

    시간표에 색 입히는 기능 추가

--- app/src/main/java/com/example/son/timetable/MainActivity.java
+++ app/src/main/java/com/example/son/timetable/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase db;
     String tag = "SQLite";
 
-    Button.OnClickListener tableClickListener = new View.OnClickListener(){
+    View.OnClickListener tableClickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
             String button_id = getResources().getResourceName(v.getId());
@@ -40,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
             //insert(values[0], values[1], "테스트");
         }
     };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,7 +57,13 @@ public class MainActivity extends AppCompatActivity {
             Log.e(tag, "데이터베이스를 얻어올 수 없음");
             finish();
         }
-
+        /////////////////////////// 나중에 지우기
+        String sql = "drop table timetable";
+        db.execSQL(sql);
+        sql = "create table timetable(id INTEGER PRIMARY KEY AUTOINCREMENT, class TEXT, day TEXT, lecture TEXT, color INTEGER);";
+        db.execSQL(sql);
+        ///////////////////////////
+        
         select();
         updateTimeTable();
         registButtonFunc();
@@ -66,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
     void showTimeTableDialog(final String _class, final String day) // 입력창 띄우기
     {
+        /*
         final EditText edittext = new EditText(this);
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
         alertDialog.setTitle("시간표 입력");
@@ -88,13 +94,40 @@ public class MainActivity extends AppCompatActivity {
                 btn.setText("");
             }
         });
-        alertDialog.show();
+        alertDialog.show();*/
+        final TimeInputDialog td = new TimeInputDialog(this);
+
+        td.setPositiveButton(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                insert(_class, day, td.getLecture(), td.getTableColor());                     // 두번 넣을경우 다 들어간다 중복처리 해줘야함.
+                updateTimeTable();
+                td.dismiss();
+            }
+        });
+        td.setNegativeButton(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                td.dismiss();
+            }
+        });
+        td.setDeleteButton(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                db.execSQL("delete from timetable where class='"+_class+"' and day='"+day+"'");
+                String button_id = _class +"_"+day;
+                Button btn = (Button)findViewById(getResources().getIdentifier(button_id, "id", getPackageName()));
+                btn.setText("");
+                btn.setBackgroundColor(Color.WHITE);
+            }
+        });
+        td.show();
     }
 
-    void insert(String _class, String day, String lecture) // DB에 삽입
+    void insert(String _class, String day, String lecture, int color) // DB에 삽입
     {
         if(checkDup(_class, day)==0)
-            db.execSQL("insert into timetable VALUES(null, '" + _class + "','" + day + "','" + lecture + "');");
+            db.execSQL("insert into timetable VALUES(null, '" + _class + "','" + day + "','" + lecture + "'," + color + ");");
         else
         {
             db.execSQL("update timetable set lecture='"+lecture+"' where class='"+_class+"' and day='"+day+"'");
@@ -132,12 +165,12 @@ public class MainActivity extends AppCompatActivity {
             String _class = c.getString(1);
             String day = c.getString(2);
             String lecture = c.getString(3);
-            Log.d(tag, id + " " + _class + " " + day + " " + lecture);
+            int color = c.getInt(4);
 
             String button_id = _class +"_"+day;
-            Log.d(tag, button_id);
             Button btn = (Button)findViewById(getResources().getIdentifier(button_id, "id", getPackageName()));
             btn.setText(lecture);
+            btn.setBackgroundColor(color);
         }
     }
 
